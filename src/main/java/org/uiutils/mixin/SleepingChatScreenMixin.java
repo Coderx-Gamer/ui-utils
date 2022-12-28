b9f029ebcedec8f5504b3d539890c1b34dd36008
Kay Alizer <64274699+iAmSpace@users.noreply.github.com>
2022-12-28 23:53:26 +0000

    Update SleepingChatScreenMixin.java

--- src/main/java/org/uiutils/mixin/SleepingChatScreenMixin.java
+++ src/main/java/org/uiutils/mixin/SleepingChatScreenMixin.java
@@ -15,17 +15,18 @@ public class SleepingChatScreenMixin extends Screen {
         super(title);
     }
 
-
     // called when SleepingChatScreen is created
     @Inject(at = @At("TAIL"), method = "init")
     public void init(CallbackInfo ci) {
 
         // register "client wake up" button for SleepingChatScreen
-        addDrawableChild(new ButtonWidget(5, 5, 160, 20, Text.of("Client wake up"), (button) -> {
-
+        this.addDrawableChild(ButtonWidget.builder(Text.of("Client wake up"), (button) -> {
             // wakes the player up client-side
-            client.player.wakeUp();
-            client.setScreen(null);
-        }));
+            if (this.client != null && this.client.player != null) {
+                // I have implemented these checks to stop IntelliJ crying
+                this.client.player.wakeUp();
+                this.client.setScreen(null);
+            }
+        }).dimensions(5, 5, 160, 20).build());
     }
 }
