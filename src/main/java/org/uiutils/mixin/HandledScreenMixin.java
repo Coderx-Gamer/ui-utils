c53e9cd0c5d14d53fb6259dd870f2aad31c59341
Patrick <doritkastner00@gmail.com>
2023-05-25 21:58:01 +0200

    Gray-out "Copy GUI Title JSON"

--- src/main/java/org/uiutils/mixin/HandledScreenMixin.java
+++ src/main/java/org/uiutils/mixin/HandledScreenMixin.java
@@ -371,15 +371,18 @@ public class HandledScreenMixin extends Screen {
                 frame.setVisible(true);
             }).width(120).position(5, 185).build());
 
-            if(SharedVariables.osIsMac) fabricatePacket.active = false;
-
-            addDrawableChild(ButtonWidget.builder(Text.of("Copy GUI Title JSON"), (button) -> {
+            ButtonWidget copyGuiTitleJon =  addDrawableChild(ButtonWidget.builder(Text.of("Copy GUI Title JSON"), (button) -> {
                 try {
                     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(Text.Serializer.toJson(mc.currentScreen.getTitle())), null);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }).width(120).position(5, 215).build());
+
+            if(SharedVariables.osIsMac) {
+                fabricatePacket.active = false;
+                copyGuiTitleJon.active = false;
+            }
         }
     }
 
